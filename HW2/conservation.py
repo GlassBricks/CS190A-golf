4b45ea46ba2044bf22d9088f7ea2a1c92f1307f1
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-02-01 22:52:50 -0800

    Improve conservation.py even more

--- HW2/conservation.py
+++ HW2/conservation.py
@@ -1,11 +1,11 @@
 R=lambda:[*map(int,input().split())]
 def t(l):
-	Q={0},{0};d=[*D]
+	Q={0},{0};*d,=D
 	while q:=Q[l&1]:
 		for u in c[q.pop()]:d[u]-=1;d[u]or Q[L[u-1]<2].add(u)
 		l+=not q
 	return l
 for _ in[0]*R()[0]:
-	n,m=R();n+=1;D=[2]*n;c=[[]for _ in D];L=R();c[0]=range(1,n)
-	for _ in[0]*m:a,b=R();c[a]+=[b];D[b]+=1
+	n,m=R();D=[2]*-~n;L=R();c=[range(1,-~n)]+[[]for _ in D]
+	for _ in[0]*m:a,b=R();c[a]+=b,;D[b]+=1
 	print(min(t(-2),t(-1)-1))
