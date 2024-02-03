00009e7298de84b92225f5042d73ceab46a420f8
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-02-03 10:43:46 -0800

    Improve faulty robot by 1 char

--- HW3/robot.py
+++ HW3/robot.py
@@ -2,5 +2,5 @@ R=lambda:map(int,input().split())
 n,m=R()
 g={i-n:[R]for i in range(3*n)}
 for v in[{0}]*m:a,b=R();g[a]+=b,
-V=lambda c,b=0:c==R or(c in v)^1and(v.add(c)or V(g[-c][-1],b)+(b and sum(map(V,g[c][1:]))))
+V=lambda c,b=0:c==R or(c in v)^1and(V(g[-c][v.add(c)or-1],b)+(b and sum(map(V,g[c][1:]))))
 print(V(1,1))
