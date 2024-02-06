b31b197e448e3b93a52c9e4842319545e2c959d9
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-02-05 18:32:11 -0800

    Improve texas.py

--- HW3/texas.py
+++ HW3/texas.py
@@ -3,8 +3,8 @@ D=[(1e9,0)]*-~n+[(0,0)]
 Z={*range(n+2)}
 P=[complex(*map(int,input().split()))for _ in D]
 while Z:
-    d,c=min((D[i][0],i)for i in Z);Z-={c}
-    for i in Z:D[i]=min(D[i],(d+abs(P[i]-P[c])**2,c))
+	d,c=min((D[i][0],i)for i in Z);Z-={c}
+	for i in Z:D[i]=min(D[i],(d+abs(P[i]-P[c])**2,c))
 c=n
-if D[c][1]>c:exit(print('-'))
+D[c][1]>c==exit(print('-'))
 while(c:=D[c][1])<n:print(c)
