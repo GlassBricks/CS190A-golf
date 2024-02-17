ecd945484a00cde2cbfa93a76b39be735ddc6a94
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-02-17 15:51:42 -0800

    Improve bicikli

--- HW5/bicikli.py
+++ HW5/bicikli.py
@@ -1,13 +1,12 @@
 R=lambda:map(int,input().split())
 n,I=R()
-d=[0,0]*n
+d=[0]*-~n
 a=[0,1]+d
 g=[[]for _ in d]
-b=[[]for _ in d]
-for _ in[0]*I:x,y=R();g[x]+=y,;b[y]+=x,
-for s,c in(1,g),(2,b):
-	q={s};O=I;I={0}
-	while q:v=q.pop();I&{v}or[*map(q.add,c[v])];I|={v}
+for b in[[[]for _ in d]]*I:x,y=R();g[x]+=y,;b[y]+=x,
+for c,*q in(g,1),(b,2):
+	O=I;I={0}
+	while q:v=q.pop();q+=c[v]*len({v}-I);I|={v}
 for i in I&O:
 	for j in g[i]:d[j]+=1
 q={1}
